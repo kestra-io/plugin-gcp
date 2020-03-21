62645fc01bc43090283bcec8b0084a37a24867be
Lucas Deleforge <50104033+lucasDeleforge@users.noreply.github.com>
2020-03-21 14:15:48 +0100

    feat(bigquery): ExtractToGcs BigQuery task
    relate to #3

---
+++ src/main/java/org/kestra/task/gcp/bigquery/ExtractToGcs.java
@@ -0,0 +1,231 @@
+package org.kestra.task.gcp.bigquery;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.JobStatistics;
+import com.google.cloud.bigquery.ExtractJobConfiguration;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.kestra.core.exceptions.IllegalVariableEvaluationException;
+import org.kestra.core.models.annotations.Documentation;
+import org.kestra.core.models.annotations.Example;
+import org.kestra.core.models.annotations.InputProperty;
+import org.kestra.core.models.annotations.OutputProperty;
+import org.kestra.core.models.tasks.RunnableTask;
+import org.kestra.core.models.executions.metrics.Counter;
+import org.kestra.core.models.executions.metrics.Timer;
+import org.kestra.core.runners.RunContext;
+import org.kestra.core.serializers.JacksonMapper;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.time.Duration;
+
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Example(
+    title = "Extract a BigQuery table to a gcs bucket",
+    code = {
+        "destinationUris: gs://bucket_name/filename.csv",
+        "sourceTable: \"my_project.my_dataset.my_table\"",
+        "format: CSV",
+        "fieldDelimiter: ';'",
+        "printHeader: true"
+    }
+)
+@Documentation(
+    description = "Extract data from BigQuery table to GCS (Google Cloud Storage)"
+)
+public class ExtractToGcs extends AbstractBigquery implements RunnableTask<ExtractToGcs.Output>{
+
+    @InputProperty(
+        dynamic = true,
+        description = "The table to export."
+    )
+    private String sourceTable;
+
+    @InputProperty(
+        dynamic = true,
+        description = "The list of fully-qualified Google Cloud Storage URIs (e.g. gs://bucket/path) where " +
+            "the extracted table should be written."
+    )
+    private List<String> destinationUris;
+
+    @InputProperty(
+        dynamic = true,
+        description = "the compression value to use for exported files. If not set exported files " +
+            "are not compressed. "
+    )
+    private String compression;
+
+    @InputProperty(
+        dynamic = true,
+        description = "The delimiter to use between fields in the exported data. By default \",\" is used."
+    )
+    private String fieldDelimiter;
+
+    @InputProperty(
+        dynamic = true,
+        description = "The exported file format. If not set table is exported in CSV format. "
+    )
+    private String format;
+
+    @InputProperty(
+        description = "[Optional] Flag if format is set to \"AVRO\".",
+        body = {"[Optional] If destinationFormat is set to \"AVRO\", this flag indicates whether to enable extracting " +
+            "applicable column types (such as TIMESTAMP) to their corresponding AVRO logical " +
+            "types (timestamp-micros), instead of only using their raw types (avro-long).",
+            "Parameters:",
+            "    useAvroLogicalTypes - useAvroLogicalTypes or null for none "}
+    )
+    private Boolean useAvroLogicalTypes;
+
+    @InputProperty(
+        description = "[Optional] Job timeout in milliseconds. If this time limit is exceeded, " +
+            "BigQuery may attempt to terminate the job."
+    )
+    private Long jobTimeoutMs;
+
+    @InputProperty(
+        description = "The labels associated with this job.",
+        body = {"The labels associated with this job. You can use these to organize and group your jobs. Label " +
+            "keys and values can be no longer than 63 characters, can only contain lowercase letters, " +
+            "numeric characters, underscores and dashes. International characters are allowed. Label " +
+            "values are optional. Label keys must start with a letter and each label in the list must have " +
+            "a different key.",
+            "Parameters:",
+            "    labels - labels or null for none "},
+        dynamic = true
+    )
+    private Map<String,String> labels;
+
+    @InputProperty(
+        description = "Whether to print out a header row in the results. By default an header is printed."
+    )
+    private Boolean printHeader;
+
+    @Override
+    public ExtractToGcs.Output run(RunContext runContext) throws Exception {
+        BigQuery connection = this.connection(runContext);
+        Logger logger = runContext.logger(this.getClass());
+
+        ExtractJobConfiguration configuration = this.buildExtractJob(runContext);
+
+        Job extractJob = connection.create(JobInfo.of(configuration));
+
+        logger.debug("Starting query\n{}", JacksonMapper.log(configuration));
+
+        return this.execute(runContext, logger, configuration, extractJob);
+    }
+
+    protected ExtractToGcs.Output execute(RunContext runContext, Logger logger, ExtractJobConfiguration configuration, Job job) throws InterruptedException, IOException, IllegalVariableEvaluationException {
+        Connection.handleErrors(job, logger);
+        job = job.waitFor();
+        Connection.handleErrors(job, logger);
+
+        JobStatistics.ExtractStatistics stats = job.getStatistics();
+        this.metrics(runContext, stats, job);
+
+        return Output.builder()
+            .jobId(job.getJobId().getJob())
+            .sourceTable(configuration.getSourceTable().getTable())
+            .destinationUris(configuration.getDestinationUris())
+            .fileCounts(stats.getDestinationUriFileCounts())
+            .build();
+    }
+
+    @Builder
+    @Getter
+    public static class Output implements org.kestra.core.models.tasks.Output {
+        @OutputProperty(
+            description = "The job id"
+        )
+        private String jobId;
+
+        @OutputProperty(
+            description = "source Table"
+        )
+        private String sourceTable;
+
+        @OutputProperty(
+            description = "The destination URI file"
+        )
+        private List<String> destinationUris;
+
+        @OutputProperty()
+        private List<Long> fileCounts;
+    }
+
+    private void metrics(RunContext runContext, JobStatistics.ExtractStatistics stats, Job job) throws IllegalVariableEvaluationException {
+        String[] tags = {
+            "source_table", runContext.render(this.sourceTable),
+            "project_id", job.getJobId().getProject(),
+            "location", job.getJobId().getLocation(),
+        };
+
+        if (stats.getDestinationUriFileCounts() != null) {
+            // Sum of the number of files extracted
+            long fileCounts = stats.getDestinationUriFileCounts().stream().mapToLong(Long::longValue).sum();
+            runContext.metric(Counter.of("output.file_counts", fileCounts, tags));
+        }
+
+        runContext.metric(Timer.of("duration", Duration.ofNanos(stats.getEndTime() - stats.getStartTime()), tags));
+    }
+
+    protected ExtractJobConfiguration buildExtractJob(RunContext runContext) throws IllegalVariableEvaluationException {
+        ExtractJobConfiguration.Builder builder = ExtractJobConfiguration
+            .newBuilder(Connection.tableId(
+                runContext.render(this.sourceTable)),
+                runContext.render(this.destinationUris));
+
+        if (runContext.render(this.sourceTable) != null){
+            builder.setSourceTable(Connection.tableId(runContext.render(this.sourceTable)));
+        }
+
+        if (runContext.render(this.destinationUris) != null){
+            builder.setDestinationUris(runContext.render(this.destinationUris));
+        }
+
+        if (runContext.render(this.compression) != null) {
+            builder.setCompression(runContext.render(runContext.render(this.compression)));
+        }
+
+        if (runContext.render(this.fieldDelimiter) != null) {
+            builder.setFieldDelimiter(runContext.render(this.fieldDelimiter));
+        }
+
+        if (runContext.render(this.format) != null) {
+            builder.setFormat(runContext.render(this.format));
+        }
+
+        if (this.printHeader != null) {
+            builder.setPrintHeader(this.printHeader);
+        }
+
+        if (this.jobTimeoutMs != null) {
+            builder.setJobTimeoutMs(this.jobTimeoutMs);
+        }
+
+        if (this.useAvroLogicalTypes != null) {
+            builder.setUseAvroLogicalTypes(this.useAvroLogicalTypes);
+        }
+
+        if (this.labels != null) {
+            builder.setLabels(this.labels);
+        }
+
+        return builder.build();
+    }
+}
+
---
+++ src/test/java/org/kestra/task/gcp/bigquery/ExtractToGcsTest.java
@@ -0,0 +1,132 @@
+package org.kestra.task.gcp.bigquery;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.*;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.CharStreams;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.kestra.core.runners.RunContext;
+import org.kestra.core.utils.TestsUtils;
+import org.kestra.core.models.tasks.Task;
+import org.kestra.core.storages.StorageInterface;
+import org.kestra.task.gcp.gcs.Download;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.UUID;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+public class ExtractToGcsTest extends AbstractBigquery{
+    @Inject
+    private ApplicationContext applicationContext;
+
+    @Inject
+    private StorageInterface storageInterface;
+
+    @Value("${kestra.tasks.bigquery.project}")
+    private String project;
+
+    @Value("${kestra.tasks.bigquery.dataset}")
+    private String dataset;
+
+    @Value("${kestra.tasks.bigquery.table}")
+    private String table;
+
+    @Value("${kestra.tasks.gcs.bucket}")
+    private String bucket;
+
+    @Value("${kestra.tasks.gcs.filename}")
+    private String filename;
+
+    @Value("true")
+    private Boolean printHeader;
+
+    private BigQuery connection;
+
+    @BeforeEach
+    private void init()  {
+        this.connection = new Connection().of(projectId, "EU");
+    }
+
+    private Job query(String query) throws InterruptedException {
+        return this.connection
+            .create(JobInfo
+                .newBuilder(QueryJobConfiguration.newBuilder(query).build())
+                .setJobId(JobId.of(UUID.randomUUID().toString()))
+                .build()
+            )
+            .waitFor();
+    }
+
+    @Test
+    void toCsv() throws Exception {
+        // Sample table
+        query("CREATE OR REPLACE TABLE  `" + this.dataset + "." +  this.table + "`" +
+            "(product STRING, quantity INT64)" +
+            ";" +
+            "INSERT `" + this.dataset + "." +  this.table + "` (product, quantity)" +
+            "VALUES('top load washer', 10)" +
+            ";");
+
+        // Extract task
+        ExtractToGcs task = ExtractToGcs.builder()
+            .id(ExtractToGcsTest.class.getSimpleName())
+            .type(ExtractToGcs.class.getName())
+            .destinationUris(Collections.singletonList(
+                "gs://" + this.bucket + "/" + this.filename
+            ))
+            .sourceTable(this.project + "." + this.dataset + "." + this.table)
+            .printHeader(printHeader)
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(applicationContext, task, ImmutableMap.of());
+        ExtractToGcs.Output extractOutput = task.run(runContext);
+
+        // Download task
+        String testString = "product,quantity\n" +
+            "top load washer,10\n";
+
+        Download downloadTask = Download.builder()
+            .id(ExtractToGcsTest.class.getSimpleName())
+            .type(Download.class.getName())
+            .from(extractOutput.getDestinationUris().get(0))
+            .build();
+
+        Download.Output downloadOutput = downloadTask.run(runContext(downloadTask));
+        InputStream get = storageInterface.get(downloadOutput.getUri());
+
+        // Tests
+        assertThat(extractOutput.getFileCounts().get(0), is(1L));
+        assertThat(
+            CharStreams.toString(new InputStreamReader(get)),
+            is(testString)
+        );
+
+        // Clean sample table
+        query("DROP TABLE  `" + this.dataset + "." +  this.table + "` ;");
+
+    }
+
+    private RunContext runContext(Task task) {
+        return TestsUtils.mockRunContext(
+            this.applicationContext,
+            task,
+            ImmutableMap.of(
+                "bucket", this.bucket
+            )
+        );
+    }
+}
+
