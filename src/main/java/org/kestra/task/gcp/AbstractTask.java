5516a6f25edec6de8e491efdad090ffa2ae05a39
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2021-01-05 10:12:58 +0100

    feat(tasks): log service account beeing used

--- src/main/java/org/kestra/task/gcp/AbstractTask.java
+++ src/main/java/org/kestra/task/gcp/AbstractTask.java
@@ -1,20 +1,21 @@
 package org.kestra.task.gcp;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.kestra.core.exceptions.IllegalVariableEvaluationException;
-import org.kestra.core.models.annotations.PluginProperty;
 import org.kestra.core.models.tasks.Task;
 import org.kestra.core.runners.RunContext;
+import org.kestra.core.serializers.JacksonMapper;
+import org.slf4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @SuperBuilder
 @ToString
@@ -31,11 +32,21 @@ public abstract class AbstractTask extends Task implements GcpInterface {
 
     protected GoogleCredentials credentials(RunContext runContext) throws IllegalVariableEvaluationException, IOException {
         GoogleCredentials credentials;
-        
+
+
         if (serviceAccount != null) {
             String serviceAccount = runContext.render(this.serviceAccount);
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serviceAccount.getBytes());
             credentials = ServiceAccountCredentials.fromStream(byteArrayInputStream);
+            Logger logger = runContext.logger();
+
+            if (logger.isTraceEnabled()) {
+                byteArrayInputStream.reset();
+                Map<String, String> jsonKey = JacksonMapper.ofJson().readValue(byteArrayInputStream, new TypeReference<>() {});
+                if (jsonKey.containsKey("client_email")) {
+                    logger.trace(" • Using service account: {}", jsonKey.get("client_email") );
+                }
+            }
         } else {
             credentials = GoogleCredentials.getApplicationDefault();
         }
