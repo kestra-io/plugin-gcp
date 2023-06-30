20634de2dbfbfc91a20907c7daa94cfc9c7bdeb0
Loïc Mathieu <loikeseke@gmail.com>
2023-06-30 10:01:33 +0200

    fix: trigger tests

--- src/test/java/io/kestra/plugin/gcp/pubsub/TriggerTest.java
+++ src/test/java/io/kestra/plugin/gcp/pubsub/TriggerTest.java
@@ -63,13 +63,13 @@ class TriggerTest {
         CountDownLatch queueCount = new CountDownLatch(1);
 
         // scheduler
+        Worker worker = new Worker(applicationContext, 8, null);
         try (
             AbstractScheduler scheduler = new DefaultScheduler(
                 this.applicationContext,
                 this.flowListenersService,
                 this.triggerState
             );
-            Worker worker = new Worker(applicationContext, 8, null);
         ) {
             AtomicReference<Execution> last = new AtomicReference<>();
 
