dd817149805760a3e184704d9f3025ad32c8c080
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-06-26 22:52:35 +0200

    feat(batch): add ComputeResource configurations
    close #409

--- src/main/java/io/kestra/plugin/gcp/runner/Batch.java
+++ src/main/java/io/kestra/plugin/gcp/runner/Batch.java
@@ -127,6 +127,16 @@ public class Batch extends TaskRunner implements GcpInterface, RemoteRunnerInter
     private String machineType = "e2-medium";
 
     @Schema(
+        title = "Compute resource requirements.",
+        description = "ComputeResource defines the amount of resources required for each task. Make sure your tasks " +
+            "have enough resources to successfully run. If you also define the types of resources for a job to use with the " +
+            "[InstancePolicyOrTemplate](https://cloud.google.com/batch/docs/reference/rest/v1/projects.locations.jobs#instancepolicyortemplate) " +
+            "field, make sure both fields are compatible with each other."
+    )
+    @PluginProperty
+    private ComputeResource computeResource;
+
+    @Schema(
         title = "Compute reservation."
     )
     @PluginProperty(dynamic = true)
@@ -252,6 +262,24 @@ public class Batch extends TaskRunner implements GcpInterface, RemoteRunnerInter
 
                 var taskBuilder = TaskSpec.newBuilder();
                 taskBuilder.setMaxRunDuration(com.google.protobuf.Duration.newBuilder().setSeconds(waitDuration.getSeconds()));
+
+                if (this.computeResource != null) {
+                    com.google.cloud.batch.v1.ComputeResource.Builder computeResourceBuilder = com.google.cloud.batch.v1.ComputeResource.newBuilder();
+
+                    if (this.computeResource.bootDisk != null) {
+                        computeResourceBuilder.setBootDiskMib(this.computeResource.bootDisk);
+                    }
+
+                    if (this.computeResource.cpu != null) {
+                        computeResourceBuilder.setCpuMilli(this.computeResource.cpu);
+                    }
+
+                    if (this.computeResource.memory != null) {
+                        computeResourceBuilder.setMemoryMib(this.computeResource.memory);
+                    }
+
+                    taskBuilder.setComputeResource(computeResourceBuilder.build());
+                }
 
                 if (hasFilesToDownload || hasFilesToUpload || outputDirectoryEnabled) {
                     taskBuilder.addVolumes(Volume.newBuilder()
@@ -479,5 +507,49 @@ public class Batch extends TaskRunner implements GcpInterface, RemoteRunnerInter
         @Schema(title = "Subnetwork identifier with the format `projects/HOST_PROJECT_ID/regions/REGION/subnetworks/SUBNET`")
         @PluginProperty(dynamic = true)
         private String subnetwork;
+    }
+
+    @Getter
+    @Builder
+    public static class ComputeResource {
+        @Schema(
+            title = "The milliCPU count.",
+            description = "defines the amount of CPU resources per task in milliCPU units. For example, `1000` " +
+                "corresponds to 1 vCPU per task. If undefined, the default value is `2000`." +
+                "\n" +
+                "If you also define the VM's machine type using the `machineType` in " +
+                "[InstancePolicy](https://cloud.google.com/batch/docs/reference/rest/v1/projects.locations.jobs#instancepolicy) " +
+                "field or inside the `instanceTemplate` in the " +
+                "[InstancePolicyOrTemplate](https://cloud.google.com/batch/docs/reference/rest/v1/projects.locations.jobs#instancepolicyortemplate) " +
+                "field, make sure the CPU resources for both fields are compatible with each other and with how many " +
+                "tasks you want to allow to run on the same VM at the same time.\n" +
+                "\n" +
+                "For example, if you specify the `n2-standard-2` machine type, which has 2 vCPUs each, you are " +
+                "recommended to set `cpu` no more than `2000`, or you are recommended to run two tasks on the same VM " +
+                "if you set cpuMilli to `1000` or less."
+        )
+        @PluginProperty
+        private Integer cpu;
+
+        @Schema(
+            title = "Memory in MiB.",
+            description = "defines the amount of memory per task in MiB units. If undefined, the default value is `2000`. " +
+                "If you also define the VM's machine type using the `machineType` in " +
+                "[InstancePolicy](https://cloud.google.com/batch/docs/reference/rest/v1/projects.locations.jobs#instancepolicy) " +
+                "field or inside the `instanceTemplate` in the " +
+                "[InstancePolicyOrTemplate](https://cloud.google.com/batch/docs/reference/rest/v1/projects.locations.jobs#instancepolicyortemplate) " +
+                "field, make sure the memory resources for both fields are compatible with each other and with how many " +
+                "tasks you want to allow to run on the same VM at the same time.\n" +
+                "\n" +
+                "For example, if you specify the `n2-standard-2` machine type, which has 8 GiB each, you are " +
+                "recommended to set `memory` to no more than `8192`, or you are recommended to run two tasks on the " +
+                "same VM if you set `memory` to `4096` or less."
+        )
+        @PluginProperty
+        private Integer memory;
+
+        @Schema(title = "Extra boot disk size in MiB for each task.")
+        @PluginProperty
+        private Integer bootDisk;
     }
 }
