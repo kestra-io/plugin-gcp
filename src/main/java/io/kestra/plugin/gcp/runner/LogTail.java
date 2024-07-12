8114f09a464d8e4b7f872a62abbc33270b7e1011
Loïc Mathieu <loikeseke@gmail.com>
2024-07-12 13:52:51 +0200

    fix: parse JSON log

--- src/main/java/io/kestra/plugin/gcp/runner/LogTail.java
+++ src/main/java/io/kestra/plugin/gcp/runner/LogTail.java
@@ -1,17 +1,20 @@
 package io.kestra.plugin.gcp.runner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.LogEntryServerStream;
 import com.google.cloud.logging.Payload;
 import com.google.cloud.logging.Severity;
 import io.kestra.core.models.tasks.runners.AbstractLogConsumer;
+import io.kestra.core.serializers.JacksonMapper;
 
 import java.time.Duration;
-import java.util.Iterator;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.CancellationException;
 
 class LogTail implements AutoCloseable {
+    private static final ObjectMapper MAPPER = JacksonMapper.ofJson();
+
     private final LogEntryServerStream stream;
     private final Duration waitForLogInterval;
     private final Thread logThread;
@@ -22,8 +25,24 @@ class LogTail implements AutoCloseable {
 
         logThread = Thread.ofVirtual().start(
             () -> {
-                for (LogEntry entry : this.stream) {
-                    logConsumer.accept(entry.<Payload.StringPayload>getPayload().getData(), isError(entry.getSeverity()));
+                try {
+                    for (LogEntry entry : this.stream) {
+                        if (entry.getPayload() instanceof Payload.StringPayload str) {
+                            logConsumer.accept(str.getData(), isError(entry.getSeverity()));
+                        } else if (entry.getPayload() instanceof Payload.JsonPayload json) {
+                            try {
+                                String strPayload = MAPPER.writeValueAsString(json.getDataAsMap());
+                                logConsumer.accept(strPayload, isError(entry.getSeverity()));
+                            } catch (JsonProcessingException e) {
+                                logConsumer.accept("Unable to parse JSON log message: " + e.getMessage(), true);
+                            }
+                        } else {
+                            logConsumer.accept("Unable to process a log payload of type: " + entry.getPayload().getClass().getName(), true);
+                        }
+                    }
+                } catch (CancellationException e) {
+                    // when the stream is canceled, a cancellation exception is throws
+                    // we ignore it as ... well ... we cancel the stream, so we cause this
                 }
             }
         );
