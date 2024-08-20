602477f7b1f25f65c98e17c8aa0c4828fa251e2e
Nikita Hrytsenko <82543287+iNikitaGricenko@users.noreply.github.com>
2024-08-20 16:00:36 +0300

    feat: Tasks to manipulate Dataproc clusters (Create and De… (#433)
    * Implemented tasks to manipulate with Dataproc clusters (Create and Delete)
    
    * Removed redundant code. Changed title for tasks. Added authentication

---
+++ src/test/java/io/kestra/plugin/gcp/dataproc/clusters/CreateTest.java
@@ -0,0 +1,51 @@
+package io.kestra.plugin.gcp.dataproc.clusters;
+
+import autovalue.shaded.com.google.common.collect.ImmutableMap;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.TestsUtils;
+import io.micronaut.context.annotation.Value;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest
+@Slf4j
+@Disabled
+public class CreateTest {
+
+	@Inject
+	private RunContextFactory runContextFactory;
+
+	@Value("${kestra.tasks.dataproc.project}")
+	private String project;
+
+	@Value("${kestra.tasks.gcs.bucket}")
+	private String bucket;
+
+	@Value("${kestra.tasks.dataproc.region}")
+	private String region;
+
+    @Test
+    void run() throws Exception {
+        Create create = Create.builder()
+            .id(Create.class.getSimpleName())
+            .type(Create.class.getName())
+            .projectId(project)
+            .clusterName("test-cluster")
+	        .region(region)
+	        .bucket(bucket)
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, create, ImmutableMap.of());
+
+        Create.Output createOutput = create.run(runContext);
+        assertThat(createOutput.isCreated(), is(true));
+    }
+
+}
