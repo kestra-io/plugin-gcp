3af151ff9906bbd8b95a90a84d1f407a9db58ce7
Shruti Mantri <shruti1810@gmail.com>
2024-09-26 22:45:30 -0700

    fix: add full examples for dataproc cluster tasks and doc correction

--- src/main/java/io/kestra/plugin/gcp/dataproc/batches/package-info.java
+++ src/main/java/io/kestra/plugin/gcp/dataproc/batches/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
-    title = "Dataproc",
-    description = "This sub-group of plugins contains tasks for submitting batches on Google Cloud Dataproc." +
+    title = "Dataproc Batches",
+    description = "This sub-group of plugins contains tasks for submitting batches on Google Cloud Dataproc. " +
         "Dataproc is a managed Apache Spark and Apache Hadoop service that lets you take advantage of open source data tools for batch processing, querying, streaming, and machine learning.",
     categories = { PluginSubGroup.PluginCategory.BATCH, PluginSubGroup.PluginCategory.CLOUD }
 )
--- src/main/java/io/kestra/plugin/gcp/dataproc/clusters/package-info.java
+++ src/main/java/io/kestra/plugin/gcp/dataproc/clusters/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
-    title = "Dataproc",
-    description = "This sub-group of plugins contains tasks to manipulate clusters on Google Cloud Dataproc." +
+    title = "Dataproc Clusters",
+    description = "This sub-group of plugins contains tasks to manipulate clusters on Google Cloud Dataproc. " +
         "Dataproc is a managed Apache Spark and Apache Hadoop service that lets you take advantage of open source data tools for batch processing, querying, streaming, and machine learning.",
     categories = { PluginSubGroup.PluginCategory.CLOUD }
 )
