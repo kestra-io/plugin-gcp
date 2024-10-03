9bcd17b6cac9f4f499ad85ff033dd588b9c7a755
Mathieu Gabelle <mgabelle@kestra.io>
2024-10-03 14:25:28 +0200

    feat(gcp): add gcp function http

---
+++ src/main/java/io/kestra/plugin/gcp/function/package-info.java
@@ -0,0 +1,9 @@
+@PluginSubGroup(
+    title = "Google Cloud Function",
+    description = "This sub-group of plugins contains tasks for triggering Google Cloud Function.\n" +
+        "Cloud Run functions automatically manages and scales underlying infrastructure with the size of workload. Deploy your code and let Google run and scale it for you. ",
+    categories = { PluginSubGroup.PluginCategory.TOOL, PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.gcp.function;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
