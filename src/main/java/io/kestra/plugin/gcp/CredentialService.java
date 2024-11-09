09edf046dff7cf4db76f44b40ade536c2632ab5e
Ben Poppy <me@benpoppy.dev>
2024-11-09 21:14:32 +0000

    feat(gcp): allow ability to impersonate a service account

--- src/main/java/io/kestra/plugin/gcp/CredentialService.java
+++ src/main/java/io/kestra/plugin/gcp/CredentialService.java
@@ -3,6 +3,7 @@ package io.kestra.plugin.gcp;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.auth.oauth2.ImpersonatedCredentials;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.serializers.JacksonMapper;
@@ -10,37 +11,48 @@ import org.slf4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public final class CredentialService {
-    private CredentialService() {
+  private CredentialService() {
+  }
+
+  public static GoogleCredentials credentials(RunContext runContext, GcpInterface gcpInterface)
+      throws IllegalVariableEvaluationException, IOException {
+    GoogleCredentials credentials;
+
+    HashMap<String, String> serviceAccount = gcpInterface.getServiceAccount();
+
+    if (serviceAccount.get("key") != null) {
+      String serviceAccountJson = runContext.render(serviceAccount.get("key"));
+      ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serviceAccountJson.getBytes());
+      credentials = ServiceAccountCredentials.fromStream(byteArrayInputStream);
+      Logger logger = runContext.logger();
+
+      if (logger.isTraceEnabled()) {
+        byteArrayInputStream.reset();
+        Map<String, String> jsonKey = JacksonMapper.ofJson().readValue(byteArrayInputStream, new TypeReference<>() {
+        });
+        if (jsonKey.containsKey("client_email")) {
+          logger.trace(" • Using service account: {}", jsonKey.get("client_email"));
+        }
+      }
+    } else {
+      credentials = GoogleCredentials.getApplicationDefault();
     }
 
-    public static GoogleCredentials credentials(RunContext runContext, GcpInterface gcpInterface) throws IllegalVariableEvaluationException, IOException {
-        GoogleCredentials credentials;
-
-
-        if (gcpInterface.getServiceAccount() != null) {
-            String serviceAccount = runContext.render(gcpInterface.getServiceAccount());
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serviceAccount.getBytes());
-            credentials = ServiceAccountCredentials.fromStream(byteArrayInputStream);
-            Logger logger = runContext.logger();
-
-            if (logger.isTraceEnabled()) {
-                byteArrayInputStream.reset();
-                Map<String, String> jsonKey = JacksonMapper.ofJson().readValue(byteArrayInputStream, new TypeReference<>() {});
-                if (jsonKey.containsKey("client_email")) {
-                    logger.trace(" • Using service account: {}", jsonKey.get("client_email") );
-                }
-            }
-        } else {
-            credentials = GoogleCredentials.getApplicationDefault();
-        }
-
-        if (gcpInterface.getScopes() != null) {
-            credentials = credentials.createScoped(runContext.render(gcpInterface.getScopes()));
-        }
-
-        return credentials;
+    if (gcpInterface.getScopes() != null) {
+      credentials = credentials.createScoped(runContext.render(gcpInterface.getScopes()));
     }
+
+    if (serviceAccount.get("impersonate") != null) {
+      List<String> targetScopes = runContext.render(gcpInterface.getScopes());
+      credentials = ImpersonatedCredentials.create(credentials, serviceAccount.get("impersonate"), null, targetScopes,
+          3600);
+    }
+
+    return credentials;
+  }
 }
