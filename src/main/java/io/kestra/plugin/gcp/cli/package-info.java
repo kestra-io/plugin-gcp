e86338f9eeb2d87ef6c4a6963538e1848a9175de
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 12:27:09 +0100

    fix: add package-info.java to cli package

---
+++ src/main/java/io/kestra/plugin/gcp/cli/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks to interact with GCP CLI. ",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.gcp.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
+
