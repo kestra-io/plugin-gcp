3f9501cdc753673e5ba63af09dc8696afd5e9654
yazanateer <yazan.ateerr@gmail.com>
2025-12-08 21:04:09 +0200

    creating an QueryErrorTest to test using wiremock and make somechanges in the query.java

--- src/main/java/io/kestra/plugin/gcp/bigquery/Query.java
+++ src/main/java/io/kestra/plugin/gcp/bigquery/Query.java
@@ -19,6 +19,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.cloud.bigquery.*;
 import com.google.common.collect.ImmutableMap;
 
+import dev.failsafe.Failsafe;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.common.FetchType;
+import io.kestra.core.models.tasks.retrys.AbstractRetry;
+import io.kestra.core.models.tasks.retrys.Exponential;
+import io.kestra.core.serializers.JacksonMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Metric;
@@ -307,7 +316,37 @@ public class Query extends AbstractJob implements RunnableTask<Query.Output>, Qu
             .jobId(queryJob.getJobId().getJob());
 
         if (!FetchType.NONE.equals(fetchTypeRendered)) {
-            TableResult result = queryJob.getQueryResults();
+            var retryConfig = this.getRetryAuto() != null ? this.getRetry() : Exponential.builder()
+                .type("exponential")
+                .interval(Duration.ofSeconds(5))
+                .maxInterval(Duration.ofMinutes(60))
+                .maxDuration(Duration.ofMinutes(15))
+                .maxAttempts(10)
+                .build();
+
+            TableResult result = Failsafe.with(
+                AbstractRetry.<TableResult>retryPolicy(retryConfig)
+                    .handleIf(throwable -> this.shouldRetry(throwable, logger, runContext))
+                    .onFailure(event -> logger.error(
+                        "Stop retry fetching query results, attempts {} elapsed {} seconds",
+                        event.getAttemptCount(),
+                        event.getElapsedTime().getSeconds(),
+                        event.getException()
+                    ))
+                    .onRetry(event -> logger.warn(
+                        "Retrying fetching query results, attempts {} elapsed {} seconds",
+                        event.getAttemptCount(),
+                        event.getElapsedTime().getSeconds()
+                    ))
+                    .build()
+            ).get(() -> {
+                try {
+                    return queryJob.getQueryResults();
+                } catch (com.google.cloud.bigquery.BigQueryException e) {
+                    throw new BigQueryException(e.getErrors());
+                }
+            });
+
             String[] tags = this.tags(queryJobStatistics, queryJob, fetchTypeRendered);
 
             runContext.metric(Counter.of("total.rows", result.getTotalRows(), tags));
@@ -337,6 +376,7 @@ public class Query extends AbstractJob implements RunnableTask<Query.Output>, Qu
                 }
             }
         }
+
 
         if (tableIdentity != null) {
             DestinationTable destinationTable = new DestinationTable(tableIdentity.getProject(), tableIdentity.getDataset(), tableIdentity.getTable());
