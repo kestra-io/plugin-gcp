3de801668062ef43447d14eca620e59c64dbb921
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-16 07:14:36 +0100

    chore: update plugin categories (#579)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    * Update gradle.properties
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/gcp/function/package-info.java
+++ src/main/java/io/kestra/plugin/gcp/function/package-info.java
@@ -2,7 +2,7 @@
     title = "Google Cloud Function",
     description = "This sub-group of plugins contains tasks for triggering Google Cloud Function.\n" +
         "Cloud Run functions automatically manages and scales underlying infrastructure with the size of workload. Deploy your code and let Google run and scale it for you. ",
-    categories = { PluginSubGroup.PluginCategory.TOOL, PluginSubGroup.PluginCategory.CLOUD }
+        categories = { PluginSubGroup.PluginCategory.CLOUD }
 )
 package io.kestra.plugin.gcp.function;
 
