973a5c8268e81ce9e8f6856c1cbb49b91df473c6
Malay Dewangan <malaydewangan310@gmail.com>
2026-03-12 11:26:36 +0530

    fix(bigquery): npe when formatting errors

--- src/main/java/io/kestra/plugin/gcp/bigquery/BigQueryException.java
+++ src/main/java/io/kestra/plugin/gcp/bigquery/BigQueryException.java
@@ -1,6 +1,8 @@
 package io.kestra.plugin.gcp.bigquery;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.google.cloud.bigquery.BigQueryError;
 
@@ -11,12 +13,15 @@ public class BigQueryException extends Exception {
     private final List<BigQueryError> errors;
 
     BigQueryException(List<BigQueryError> errors) {
-        super(
-            "Bigquery Errors\n[ - " +
-                String.join("\n - ", errors.stream().map(BigQueryError::toString).toArray(String[]::new)) +
-                "\n]"
-        );
+        super(formatErrors(Objects.requireNonNullElse(errors, List.of())));
+        this.errors = Objects.requireNonNullElse(errors, List.of());
+    }
 
-        this.errors = errors;
+    private static String formatErrors(List<BigQueryError> errors) {
+        return "Bigquery Errors\n[ - " +
+            errors.stream()
+                .map(BigQueryError::toString)
+                .collect(Collectors.joining("\n - ")) +
+            "\n]";
     }
 }
