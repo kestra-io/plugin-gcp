d13086672bbfc27a283bc2b41b052190fd19134b
François Delbrayelle <fdelbrayelle@kestra.io>
2026-05-12 15:55:03 +0200

    chore(ui): add oxlint & oxfmt and apply formatting to all files

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,7 +4,7 @@ import { SLOTS } from "@kestra-io/artifact-sdk";
 </script>
 
 <template>
-  <div style="padding: 1rem; font-family: sans-serif">
-    <BigqueryQueryTopologyDetails v-bind="SLOTS['topology-details'].defaultProps" />
-  </div>
+    <div style="padding: 1rem; font-family: sans-serif">
+        <BigqueryQueryTopologyDetails v-bind="SLOTS['topology-details'].defaultProps" />
+    </div>
 </template>
